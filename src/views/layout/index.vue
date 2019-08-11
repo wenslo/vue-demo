980be5acc6190872d86b76f179d427791bde67d5
wenhailin <564544234@qq.com>
2019-08-11 16:52:57 +0800

    test

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,19 +1,53 @@
 <template>
 	<div>
-		<el-container direction="vertical">
+		<el-header>
+			<el-col :span="24" class="header">
+				<el-col :span="5" class="logo" :class="'logo-width'">
+					<a href="/" style="text-decoration:none;color:#000;">
+						后台管理系统
+					</a>
+				</el-col>
+			</el-col>
+		</el-header>
+		<el-container>
+			<el-aside width="200px">
+				<el-menu
+					@open="handleOpen"
+					@close="handleClose">
+					<el-submenu index="1">
+						<template slot="title">系统管理</template>
+						<el-menu-item index="1-1">用户管理</el-menu-item>
+						<el-menu-item index="1-2">角色管理</el-menu-item>
+						<el-menu-item index="1-3">机构管理</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			</el-aside>
 			<el-main>
-				<div class="hm-layout-cell pt-20 pb-20">
-					<transition name="el-fade-in">
-						<router-view></router-view>
-					</transition>
-				</div>
+				<transition name="el-fade-in">
+					<router-view></router-view>
+				</transition>
 			</el-main>
 		</el-container>
+		<el-footer></el-footer>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'index'
+	name: 'index',
+	data () {
+		return {
+			username: '',
+			password: '',
+			rememberMe: true
+		}
+	}
 }
 </script>
+<style>
+
+	.el-footer{
+		background-color: #67c23a;
+	}
+
+</style>
